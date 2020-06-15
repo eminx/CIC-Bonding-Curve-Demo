b134c9ef123b5caac0bc850c17992b89b6816585
Emin Durak <emin@tuta.io>
2020-06-15 15:38:11 +0530

    made states better and spared into componens

--- src/App.js
+++ src/App.js
@@ -43,19 +43,24 @@ const AppBar = (props) => (
   />
 );
 
-const defaultValues = {
-  reserve: 100,
-  supply: 400,
+const defaultInitials = {
+  reserve: 100000,
+  supply: 400000,
   trr: 0.25,
 };
 
 function App() {
-  const [reserve, setReserve] = useState(defaultValues.reserve);
-  const [supply, setSupply] = useState(defaultValues.supply);
-  const [trr, setTrr] = useState(defaultValues.trr);
+  const [initials, setInitials] = useState(defaultInitials);
+  const [initMode, setInitMode] = useState(true);
 
-  const handleChange = (event) => {
-    console.log(event);
+  const setInitial = (initial) => {
+    setInitials({ ...initials, ...initial });
+  };
+
+  const initialsUIProps = {
+    initials,
+    setInitial,
+    initMode,
   };
 
   return (
@@ -65,76 +70,76 @@ function App() {
           <Heading level={2} textAlign="center">
             Interactive ABC Demo
           </Heading>
-          <Text level={5}>ABC = Augmented Bonding Curve</Text>
+          <Text>ABC = Augmented Bonding Curve</Text>
         </AppBar>
         <Box alignSelf="center" width="xxlarge" pad="medium" direction="row">
-          <Box width="40%">
-            <Form
-              onChange={(value) => console.log(value)}
-              value={{ reserve: 221 }}
-            >
-              <FormField
-                name="reserve"
-                label="Collateral Reserve"
-                margin={{ bottom: 'large' }}
-              >
-                <NumberInput
-                  size="large"
-                  value={reserve}
-                  onChange={({ target: { value } }) => setReserve(value)}
-                  step={10}
-                  thousandsSeparatorSymbol=" "
-                />
-              </FormField>
-
-              <FormField
-                name="supply"
-                label="Supply of CIC Tokens"
-                margin={{ bottom: 'large' }}
-              >
-                <NumberInput
-                  size="large"
-                  value={supply}
-                  onChange={({ target: { value } }) => setSupply(value)}
-                  step={10}
-                  thousandsSeparatorSymbol=" "
-                />
-              </FormField>
-
-              <FormField
-                name="trr"
-                label="Target Reserve Ratio"
-                margin={{ bottom: 'large' }}
-              >
-                <Box direction="row" align="center">
-                  <Box width="40%" margin={{ right: 'small' }}>
-                    <NumberInput
-                      size="large"
-                      value={trr}
-                      onChange={({ target: { value } }) => setTrr(value)}
-                      step={0.05}
-                      min={0.05}
-                      max={1}
-                    />
-                  </Box>
-                  <Box width="60%">
-                    <RangeInput
-                      value={trr}
-                      onChange={({ target: { value } }) => setTrr(value)}
-                      step={0.05}
-                      min={0.05}
-                      max={1}
-                      vertical
-                    />
-                  </Box>
-                </Box>
-              </FormField>
-            </Form>
-          </Box>
+          <InitialsUI {...initialsUIProps} />
         </Box>
       </Box>
     </Grommet>
   );
 }
+
+const InitialsUI = ({ initials, setInitial }) => (
+  <Box width="40%">
+    <Form>
+      <Field
+        name="reserve"
+        label="Collateral Reserve"
+        value={initials.reserve}
+        onChange={(value) => setInitial({ reserve: value })}
+        step={10}
+        min={1000}
+        max={1000000}
+        thousandsSeparatorSymbol=" "
+      />
+
+      <Field
+        name="supply"
+        label="Supply of CIC Tokens"
+        value={initials.supply}
+        onChange={(value) => setInitial({ supply: value })}
+        step={10}
+        min={1000}
+        max={1000000}
+        thousandsSeparatorSymbol=" "
+      />
+
+      <Field
+        name="trr"
+        label="Target Reserve Ratio"
+        value={initials.trr}
+        onChange={(value) => setInitial({ trr: value })}
+        step={0.05}
+        min={0.05}
+        max={1}
+      />
+    </Form>
+  </Box>
+);
+
+const Field = ({ name, value, label, onChange, ...otherProps }) => {
+  return (
+    <FormField name={name} label={label} margin={{ bottom: 'large' }}>
+      <Box direction="row" align="center">
+        <Box width="50%" margin={{ right: 'small' }}>
+          <NumberInput
+            size="large"
+            value={value}
+            onChange={({ target: { value } }) => onChange(value)}
+            {...otherProps}
+          />
+        </Box>
+        <Box width="50%">
+          <RangeInput
+            value={value}
+            onChange={({ target: { value } }) => onChange(value)}
+            {...otherProps}
+          />
+        </Box>
+      </Box>
+    </FormField>
+  );
+};
 
 export default App;
